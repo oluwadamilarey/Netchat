d155f86a5e466c5b4d7484931e471bb7410fdcce
hardamilare <abimbola301@gmail.com>
2021-03-19 14:22:47 +0100

    annoying bugs fixed

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -1,15 +1,13 @@
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Netchat.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Netchat.Database
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<User>
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){
-
-            public DbSet<Chat> Chats {get; set;}
-            Public DbSet
-        }
-    }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+        public DbSet<Chat> Chats {get; set;}
+        public DbSet<Message> Messages { get; set;}
+}
 }
